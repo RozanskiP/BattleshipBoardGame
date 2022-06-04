9c9cab14bd6ebf10cb4a323b8cf9aa3ddfdd9035
Pawel <pawir4@wp.pl>
2022-06-04 11:46:50 +0200

    Add basic backend

---
+++ backend/Controllers/GameController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class GameController : ControllerBase
+    {
+        public GameController()
+        {
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
